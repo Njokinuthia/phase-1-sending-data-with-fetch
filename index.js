e2047501d3241823d3e8739ad77c5a0b7f048e38
Njoki <kinuthiash@gmail.com>
2022-04-19 12:14:44 +0300

    completed assignment - test2/3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+let userName = "steve"
+let userEmail = "steve@gmail.com"
+
+function submitData(userName, userEmail) {
+  return fetch(" http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify({
+      userName,
+      userEmail
+    })
+  })
+  .then(resp=>resp.json())
+  .then(obj=>document.body.innerHTML = obj[ "id" ])
+  .catch(error=>document.body.innerHTML = error.message);
+};
+
+submitData(userName,userEmail);
+
+
